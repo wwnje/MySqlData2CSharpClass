29221bb49d43562207b421babdf71e2e2fb0ceeb
lwj <wwnje@qq.com>
2018-04-10 16:49:33 +0800

    init

---
+++ server2csharp/main.js
@@ -0,0 +1,171 @@
+var mysql = require('mysql');
+var fs = require('fs');
+var path = require('path');
+var rimraf = require('rimraf');
+var os = require('os');
+
+var OUTPUT_PATH = path.join(process.cwd(), 'output');
+
+var connection = mysql.createConnection({
+	host		: '127.0.0.1',
+	user		: 'root',
+	password	: '',
+	database	: 'pirate'
+});
+connection.connect();
+
+var tables = new Array();
+var task = 0;
+var finished = 0;
+
+function mystring(value) {
+	return "\"" + value + "\"";
+}
+
+function TaskFinish() 
+{
+	finished++;
+	if (task == finished) {
+		process.exit();
+	};
+}
+
+function ReadableName(name)
+{
+	var newname = name[0].toUpperCase() + name.substr(1);
+	var index = 0;
+	while(true)
+	{
+		index = newname.indexOf('_', index);
+		if (index == -1) {
+			break;
+		};
+
+		index++;
+		if (index >= newname.length - 1) {
+			break;
+		};
+
+		newname = newname.substr(0, index - 1) + newname[index].toUpperCase() + newname.substr(index + 1);
+	}
+	return newname;
+}
+
+function GetSpaceName(name)
+{
+	var spaceName = "";
+	if (0 == name.indexOf('c_')) {
+		spaceName = "Config";
+	}else if (0 == name.indexOf('w_')) {
+		spaceName = "World";
+	}else if (0 == name.indexOf('u_')) {
+		spaceName = "User";
+	}
+	return spaceName;
+}
+
+function OutputTable(name) {
+	task++;
+	
+	var spaceName = GetSpaceName(name);
+	var readableName = ReadableName(name.substr(2));
+	
+	var priName = "";
+
+	var sqlFindPri = 
+	'select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where table_name ='
+		+ '\'' + name + '\'' +'AND COLUMN_KEY=\'PRI\'';
+
+	connection.query(sqlFindPri,function (err, result) {
+			if(err){
+			  console.log('[SELECT ERROR] - ',err.message);
+			  return;
+			}
+			
+			for (var p in result) {
+				priName = result[p].COLUMN_NAME;
+            }  
+	});
+
+	connection.query('SHOW FULL COLUMNS FROM ' + name, function (err, rows, fields) {
+		if (err) {throw err;};
+
+		var comment = {};
+		for (var i = 0; i < rows.length; i++) {
+			comment[rows[i].Field] = rows[i].Comment;
+		};
+		
+		connection.query('SELECT * FROM ' + name, function (err, rows, fields) {
+			if (err) {throw err;};
+
+			var stringfield = {}
+
+			var contentClass = "";
+			contentClass += "namespace Game.Entity" + spaceName + os.EOL + "{" + os.EOL;
+			contentClass += "	[TableName(\"" + name + "\")]" + os.EOL;
+			contentClass += "	[PrimaryKey(\"" +  priName + "\", AutoIncrement = false)]" + os.EOL;
+			contentClass += "	public class " + readableName + os.EOL;
+			contentClass += "	{" + os.EOL;
+			for (var i = 0; i < fields.length; i++) {
+				var field = fields[i];
+				stringfield[field.name] = false;
+
+				contentClass += "		public ";
+
+				var unsighed = (field.flags & mysql.FieldFlag.UNSIGNED_FLAG) == 0 ? "" : "u";
+				
+				if (fields[i].type == mysql.Types.TINY) {
+					contentClass += 'char ';
+				}
+				else if (fields[i].type == mysql.Types.SHORT) {
+					contentClass += unsighed + 'short ';
+				}		
+				else if (fields[i].type == mysql.Types.LONG) {
+					contentClass += unsighed + 'int ';
+				}
+				else if (fields[i].type == mysql.Types.LONGLONG) {
+					contentClass += unsighed + 'long ';
+				}
+				else if (fields[i].type == mysql.Types.FLOAT || fields[i].type == mysql.Types.DOUBLE) {
+					contentClass += 'double ';
+				}
+				else if (fields[i].type == mysql.Types.VAR_STRING || fields[i].type == mysql.Types.STRING) {
+					contentClass += 'string ';
+					stringfield[field.name] = true;
+				}
+
+				contentClass += field.name + ' { get; set; } // ' + comment[field.name] + os.EOL;
+			};
+
+			contentClass += "	}" + os.EOL;
+			contentClass += "}" + os.EOL;
+
+			fs.open(path.join(OUTPUT_PATH, readableName + ".cs"), 'w+', function (ferr, fd) {
+				if (ferr) {throw ferr;};
+				fs.write(fd, contentClass);
+				TaskFinish();
+			})
+		});
+	});
+}
+
+function OnShowTables(err, rows, fields) {
+	if (err) {throw err;};
+
+	for (var i = rows.length - 1; i >= 0; i--) {
+		for (var key in rows[i]) {
+			tables.push(rows[i][key]);
+			break;
+		};
+	};
+	
+	for (var i = 0; i < tables.length; i++) {
+		OutputTable(tables[i]);
+	};
+
+	 //process.exit();
+}
+
+rimraf.sync(OUTPUT_PATH);
+fs.mkdirSync(OUTPUT_PATH);
+connection.query('SHOW TABLES', OnShowTables)
